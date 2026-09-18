3d0aeeaa1052d2902bc2a3407055d8a0b9310179
mwangangibonifaceML <mwangangiboniface581@gmail.com>
2026-09-18 15:41:09 +0700

    Update evaluation tests

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -1,21 +1,35 @@
 from __future__ import annotations
 
 import numpy as np
-from basisbench.evaluation.metrics import (mean_absolute_error,
-                                        root_mean_squared_error,
-                                        mean_square_error)
+import pytest
 
-def test_mse():
-    ypred = np.array([1,2,3,4])
-    ytrue = np.array([1,2,3,4])
-    np.isclose(mean_square_error(ypred, ytrue), 1/3)
-    
-def test_rmse():
-    ypred = np.array([1,2,3,4])
-    ytrue = np.array([1,2,3,4])
-    np.isclose(root_mean_squared_error(ypred, ytrue), 1/3)
-    
-def test_mae():
-    ypred = np.array([1,2,3,4])
-    ytrue = np.array([1,2,3,4])
-    np.isclose(mean_absolute_error(ypred, ytrue), 1/3)
+from basisbench.evaluation.metrics import (
+    mean_absolute_error,
+    mean_square_error,
+    root_mean_squared_error,
+)
+
+
+@pytest.mark.parametrize(
+    "ypred, ytrue, expected_mse, expected_rmse, expected_mae",
+    [
+        (
+            np.array([1, 2, 3, 4]),
+            np.array([0, 2, 5, 4]),
+            1.25,
+            np.sqrt(1.25),
+            0.75,
+        ),
+        (
+            np.array([1, 2, 3, 4]),
+            np.array([1, 2, 3, 4]),
+            0.0,
+            0.0,
+            0.0,
+        ),
+    ],
+)
+def test_metrics(ypred, ytrue, expected_mse, expected_rmse, expected_mae):
+    assert np.isclose(mean_square_error(ypred, ytrue), expected_mse)
+    assert np.isclose(root_mean_squared_error(ypred, ytrue), expected_rmse)
+    assert np.isclose(mean_absolute_error(ypred, ytrue), expected_mae)
